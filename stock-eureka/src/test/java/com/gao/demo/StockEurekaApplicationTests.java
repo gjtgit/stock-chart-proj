878aa8c12b38120f4c282a792db1c89d02cb6fec
JianTuanGao <JianTuanGao@DESKTOP-UI15NSI>
2019-12-15 17:36:03 +0800

    first commit

---
+++ stock-eureka/src/test/java/com/gao/demo/StockEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
